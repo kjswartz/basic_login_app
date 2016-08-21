3d6d11bac664ae91b0927e96beb12b160bcabe33
Kyle Swartz <kyle.swartz.3@gmail.com>
2016-08-21 11:45:22 -0400

    Remove api namespace

--- config/routes.rb
+++ config/routes.rb
@@ -5,17 +5,15 @@ Rails.application.routes.draw do
 
   #############################################################################
   # API
-  namespace :api, defaults: { format: :json } do
-    namespace :v1, defaults: { format: :json } do
-      # # User registration
-      # resources :registrations, only: [:create, :update]
-      # post :forgot_password, to: 'registrations#forgot_password'
-      #
-      # namespace :users do
-      #   post :sessions, to: 'sessions#create'
-      #   delete :sessions, to: 'sessions#destroy'
-      # end
-    end
+  namespace :v1, defaults: { format: :json } do
+    # # User registration
+    # resources :registrations, only: [:create, :update]
+    # post :forgot_password, to: 'registrations#forgot_password'
+    #
+    # namespace :users do
+    #   post :sessions, to: 'sessions#create'
+    #   delete :sessions, to: 'sessions#destroy'
+    # end
   end
   #############################################################################
 
